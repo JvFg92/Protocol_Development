6362c3062f0d53613b5ff13ef120c107d60d800f
JvFg92 <joaovfg34@gmail.com>
2026-05-11 09:41:02 -0300

    Change: IP to receive data from any IP address.

--- Server.py
+++ Server.py
@@ -15,7 +15,7 @@ def empacotar_mensagem(msg_type, payload):
     header = struct.pack('!BBH', 1, msg_type, length)
     return header + payload_bytes
 
-def iniciar_servidor(host='127.0.0.1', port=65432):
+def iniciar_servidor(host='0.0.0.0', port=65432):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
         s.listen()
